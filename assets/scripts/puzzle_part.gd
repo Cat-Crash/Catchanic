fcb4d116414568e4edef917a27b52ba897a49145
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-06-27 22:31:40 -0400

    enable snap to grid

--- assets/scripts/puzzle_part.gd
+++ assets/scripts/puzzle_part.gd
@@ -2,19 +2,24 @@ extends Node2D
 
 @export var moveX: bool
 @export var moveY: bool
+@export var snap: int
 
 var selected: bool = false
+var realPos: Vector2
+
+func _process(delta):
+	position = realPos.snapped(Vector2(snap, snap))
 
 func _input(event):
 	if event is InputEventMouseMotion and selected:
 		var mouseDelta: Vector2 = event.relative
-		if moveX: position.x += mouseDelta.x
-		if moveY: position.y += mouseDelta.y
+		if moveX: realPos.x += mouseDelta.x
+		if moveY: realPos.y += mouseDelta.y
 
 
 func _on_button_button_down():
 	selected = true
-
+	realPos = position
 
 func _on_button_button_up():
 	selected = false
