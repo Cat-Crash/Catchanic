facd81673f4e661b5582d2ec01f1bdf3cb15508b
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-06-28 16:30:41 -0400

    proper collision

--- assets/scripts/puzzle_part.gd
+++ assets/scripts/puzzle_part.gd
@@ -1,25 +1,31 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var moveX: bool
 @export var moveY: bool
 @export var snap: int
 
+@onready var up_cast = $AnimatableBody2D/UpCast
+@onready var down_cast = $AnimatableBody2D/DownCast
+@onready var left_cast = $AnimatableBody2D/LeftCast
+@onready var right_cast = $AnimatableBody2D/RightCast
+
+var snapVect: Vector2
 var selected: bool = false
-var realPos: Vector2
+
+func _ready():
+	snapVect = Vector2(snap, snap)
 
 func _process(delta):
-	position = realPos.snapped(Vector2(snap, snap))
-
-func _input(event):
-	if event is InputEventMouseMotion and selected:
-		var mouseDelta: Vector2 = event.relative
-		if moveX: realPos.x += mouseDelta.x
-		if moveY: realPos.y += mouseDelta.y
-
+	if not selected: return
+	
+	var currPos: Vector2 = self.global_position
+	var mousePos: Vector2 = get_global_mouse_position().snapped(snapVect)
+	
+	velocity = (mousePos - currPos) / delta
+	move_and_slide()
 
 func _on_button_button_down():
 	selected = true
-	realPos = position
 
 func _on_button_button_up():
 	selected = false
