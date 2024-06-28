9f769cd296d5d1979f4c8b906061bcf70cefe9a3
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-06-27 21:48:52 -0400

    change to relative drag method

--- assets/scripts/puzzle_part.gd
+++ assets/scripts/puzzle_part.gd
@@ -4,14 +4,10 @@ var selected: bool = false
 
 signal button_release
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if selected: global_position = get_global_mouse_position()
+func _input(event):
+	if event is InputEventMouseMotion and selected:
+		position += event.relative
 
 
 func _on_button_button_down():
