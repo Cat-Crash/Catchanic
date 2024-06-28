f6c1e3a2fbf58861ae2b60551984b29607e530ae
TABIBeck <becktiago98@gmail.com>
2024-06-27 17:39:33 -0700

    basic functionality finished, mostly commented, polishing and overworld implementation still needed

--- scripts/DialogueNode.gd
+++ scripts/DialogueNode.gd
@@ -18,17 +18,23 @@ class DialogueState: # holds everything a single piece of dialogue needs (LIST L
 	# attached to, to be set in the editor. Will be used for dialogue nametags
 static var playerName : String = "Meonthany" # Display name for the player, can be changed later
 	# but will be held consistent across all dialogues
+	
+# NOTE we still need to figure out a way to figure out how to line this up with the game world,
+# there's assignment of a placeholder in _ready() right now that just sets them to (100, 100)
+# and (300, 100) respectively
+var speakerPos : Vector2i # A vector with the x and y this needs to be over the head of the speaker
+var playerPos : Vector2i # A vector with the x and y this needs to be over the head of the player
 
 var dialoguePath : Array[DialogueState] # The DialogueStates that a conversation with this scene
 	# will go through
-
-var position : int = 0 # Current position in the dialoguePath, begins at 0
+var pathPosition : int = 0 # Current position in the dialoguePath, begins at 0
 
 var endFunction : String = 'backupFunc' # The name of the function to be called when the
 	# dialoguePath ends, will presumably be changed from the backup
 func backupFunc() -> void: # if not subbed, warns that you forgot to give it an endFunction
 	push_warning('BACKUP FUNC USED: Your dialoguePath has no end function')
 
+# Dialogue UI Elements
 @onready var dialogueArea : Control = $DialogueArea # The GUI control for the dialogue box
 @onready var dialogueBox : NinePatchRect = $DialogueArea/DialogueBox # the background for the
 	# dialogue box
@@ -37,7 +43,7 @@ func backupFunc() -> void: # if not subbed, warns that you forgot to give it an 
 @onready var dialogueText : RichTextLabel = $DialogueArea/DialogueText # The text displaying the
 	# current dialogue
 	
-# FIGURE OUT A BETTER WAY TO STORE THIS LATER
+# NOTE FIGURE OUT A BETTER WAY TO STORE THIS LATER
 var testDialoguePath : Array[DialogueState] = [
 	DialogueState.new(DialogueState.SpeakingParty.SPEAKER, "Hello there, can you hear me?"),
 	DialogueState.new(DialogueState.SpeakingParty.PLAYER, "Meow?"),
@@ -45,14 +51,16 @@ var testDialoguePath : Array[DialogueState] = [
 	DialogueState.new(DialogueState.SpeakingParty.PLAYER, "Meow meow!"),
 	DialogueState.new(DialogueState.SpeakingParty.SPEAKER, "And a mechanic, yes, I know"),
 ]
-func _ready() -> void: # just doing this on _ready() for now to make testing simpler
+func _ready() -> void: # NOTE just doing this on _ready() for now to make testing simpler
+	speakerPos = Vector2i(100, 100)
+	playerPos = Vector2i(300, 100)
 	dialoguePath = testDialoguePath
 	beginDialoguePath()
 	
 func beginDialoguePath() -> void:
 	dialogueArea.visible = true # shows the dialogue box
-	position = 0
-	updateDialogueDisplay(dialoguePath[position])
+	pathPosition = 0
+	updateDialogueDisplay(dialoguePath[pathPosition])
 
 func _on_dialogue_area_next_dialogue_signal() -> void: # When it receives the signal from DialogueArea,
 	# calls nextDialogueState()
@@ -60,10 +68,9 @@ func _on_dialogue_area_next_dialogue_signal() -> void: # When it receives the si
 
 func nextDialogueState() -> void: # Moves to the next line of dialogue in the dialoguePath. If there are no
 		# more lines, runs the end function
-	position += 1
-	print(position)
-	if position < dialoguePath.size(): # if not at the end go to the next DIalogueState in the path
-		updateDialogueDisplay(dialoguePath[position])
+	pathPosition += 1
+	if pathPosition < dialoguePath.size(): # if not at the end go to the next DIalogueState in the path
+		updateDialogueDisplay(dialoguePath[pathPosition])
 	else: # if at the end of the dialoguePath, hide the dialogue window and call the endFunction
 		dialogueArea.visible = false # hides the dialogue box
 		call(endFunction)
@@ -72,8 +79,10 @@ func updateDialogueDisplay(newState : DialogueState) -> void:
 	dialogueArea.grab_focus() # Shift the focus to the dialogueArea so that it can receive inputs
 	if newState.speakingParty == DialogueState.SpeakingParty.SPEAKER: # if speaker speaking, shows that
 		nameTag.text = speakerName
+		dialogueArea.position = speakerPos
 	elif newState.speakingParty == DialogueState.SpeakingParty.PLAYER: # if player speaking, shows that
 		nameTag.text = playerName
+		dialogueArea.position = playerPos
 	else: # if neither is speaking something has gone wrong, pushes an error
 		push_error('Undefined speaker for text, make sure that the speakingParty is set correctly in your DialogueState')
 	dialogueText.text = newState.speechText # updates the displayed text to that of the newState
