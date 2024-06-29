de048372ae8c82d3cc7b73e7105cf5aa83a0d2a2
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-06-28 22:56:04 -0400

    global gamestate signal

--- assets/scripts/puzzle/goal.gd
+++ assets/scripts/puzzle/goal.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var goal: int
 
-signal part_complete(type: Enums.PartType)
+signal part_complete(type: ProjectEnums.PartType)
 
 func _on_body_entered(body: PuzzlePart):
 	if body.target_piece and body.target_goal == goal:
--- assets/scripts/puzzle/puzzle.gd
+++ assets/scripts/puzzle/puzzle.gd
@@ -10,13 +10,13 @@ func _ready():
 	for child in get_children():
 		if child is PuzzlePart:
 			var childPart: PuzzlePart = child as PuzzlePart
-			if childPart.type == Enums.PartType.INSTALL: newParts += 1
-			elif childPart.type == Enums.PartType.UNINSTALL: oldParts += 1
+			if childPart.type == ProjectEnums.PartType.INSTALL: newParts += 1
+			elif childPart.type == ProjectEnums.PartType.UNINSTALL: oldParts += 1
 
 # Remove completed part from count and check for completion
 func _on_exit_goal_part_complete(type):
-	if type == Enums.PartType.INSTALL: newParts -= 1
-	elif type == Enums.PartType.UNINSTALL: oldParts -= 1
+	if type == ProjectEnums.PartType.INSTALL: newParts -= 1
+	elif type == ProjectEnums.PartType.UNINSTALL: oldParts -= 1
 	else: push_error("Unexpected Part Type has been Completed")
 	
 	if newParts < 1 and oldParts < 1:
