989d8ce607e8bfa192f393d695e01fcb2225d586
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-06-29 01:05:35 -0400

    update scene switch function

--- assets/scripts/globals/global_utilities.gd
+++ assets/scripts/globals/global_utilities.gd
@@ -2,12 +2,16 @@ class_name ProjectUtilities
 extends Node
 
 func switch_scene(curr_scene: Node, next_scene: Node):
-	if next_scene and "GAMESTATE" in next_scene:
+# Assumes the root is the overworld. Might have to change later.
+	if !next_scene: next_scene = get_tree().get_current_scene()
+	
+	if "GAMESTATE" in next_scene:
 		GlobalState.currentGameState = next_scene.GAMESTATE
 		next_scene.process_mode = Node.PROCESS_MODE_ALWAYS
-		next_scene.visible = false
-	else: 
-		GlobalState.currentGameState = ProjectEnums.GameState.OVERWORLD
-		
+		next_scene.visible = true
+	else: push_error("Tried to change to node without GAMESTATE")
+
 	curr_scene.process_mode = Node.PROCESS_MODE_DISABLED
-	if "visible" in curr_scene: curr_scene.visible = false
+# Don't hide the overworld
+	if "visible" in curr_scene and curr_scene.GAMESTATE != ProjectEnums.GameState.OVERWORLD: 
+		curr_scene.visible = false
