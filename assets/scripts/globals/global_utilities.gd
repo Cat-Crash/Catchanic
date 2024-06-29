5742f38f2e152793d88121f918e548a4e8f460c3
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-06-29 14:31:46 -0400

    fix puzzle piece detection

--- assets/scripts/globals/global_utilities.gd
+++ assets/scripts/globals/global_utilities.gd
@@ -15,3 +15,14 @@ func switch_scene(curr_scene: Node, next_scene: Node):
 # Don't hide the overworld
 	if "visible" in curr_scene and curr_scene.GAMESTATE != ProjectEnums.GameState.OVERWORLD: 
 		curr_scene.visible = false
+
+func get_all_children(parent: Node) -> Array[Node]:
+	var children: Array[Node] = []
+	
+	for child in parent.get_children():
+		children.append(child)
+		
+		if child.get_child_count() > 0:
+			children.append_array(get_all_children(child))
+
+	return children
