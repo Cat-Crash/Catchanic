ce42132550b141fcefcbc7011a3597be21fe200f
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-02 17:13:43 -0400

    npc scene scaffolding

---
+++ assets/scripts/npc.gd
@@ -0,0 +1,28 @@
+extends AnimatableBody3D
+
+@onready var detection_zone = $DetectionZone
+@onready var interactable_parent = $Interactables
+
+var interactables: Array[Node]
+var player_inside: bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interactables = interactable_parent.get_children()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action("interact"):
+		pass
+
+
+func _on_detection_zone_body_entered(body):
+	if body is Player: player_inside = true
+	
+
+func _on_detection_zone_body_exited(body):
+	if body is Player: player_inside = false
