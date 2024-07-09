bf57899b4a78f79c5dd5679f6292dd98a3325171
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-08 20:35:40 -0400

    replace filter function with array.assign

--- assets/scripts/npc.gd
+++ assets/scripts/npc.gd
@@ -8,13 +8,7 @@ var interactables : Array[Interactable]
 var player_inside : bool
 
 func _ready() -> void:
-	var children : Array[Node] = interactable_root.get_children() #NOTE temp
-	for child : Node in children: #NOTE temp
-		if child is Interactable:
-			interactables.append(child)
-		else:
-			push_error('non interactable under interactables')
-	#interactables = interactable_root.get_children() as Array[Interactable]
+	interactable_root.get_children().assign(interactables)
 
 func _input(event) -> void:
 	if event.is_action("interact") and player_inside:
