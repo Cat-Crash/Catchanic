d7f50b1fe6bc7557ff5255fdfaa91765ddedb1c7
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-12 18:24:40 -0400

    add accel and deccel to player movement

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,6 +2,8 @@ class_name Player
 extends CharacterBody3D
 
 @export var speed : float = 5.0
+@export var accel : float = 1
+@export var deccel : float = 2.5
 #const JUMP_VELOCITY : float = 4.5 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,13 +21,13 @@ func _physics_process(_delta : float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * speed
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = move_toward(velocity.x, direction.x, accel)
+		velocity.z = move_toward(velocity.z, direction.z, accel)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, deccel)
+		velocity.z = move_toward(velocity.z, 0, deccel)
 
 	move_and_slide()
 	GlobalState.playerPosition = global_transform.origin # set the global player position to the new one
