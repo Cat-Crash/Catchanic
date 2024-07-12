9893d69c5417f11c3c483651d2a95b48c26642b7
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-12 18:44:02 -0400

    player walk animation

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends CharacterBody3D
 
+@onready var sprite = $AnimatedSprite3D
+
 @export var speed : float = 5.0
 @export var accel : float = 1
 @export var deccel : float = 2.5
@@ -21,6 +23,10 @@ func _physics_process(_delta : float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if input_dir.x > 0: sprite.flip_h = true
+	elif input_dir.x < 0: sprite.flip_h = false
+	
 	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * speed
 	if direction:
 		velocity.x = move_toward(velocity.x, direction.x, accel)
@@ -28,6 +34,9 @@ func _physics_process(_delta : float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, deccel)
 		velocity.z = move_toward(velocity.z, 0, deccel)
+		
+	if velocity: sprite.play("walk")
+	else: sprite.play("idle")
 
 	move_and_slide()
 	GlobalState.playerPosition = global_transform.origin # set the global player position to the new one
