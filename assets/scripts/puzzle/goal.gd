9d799e6b8482ebadb4b770fa56dc08f2361d684e
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-13 00:49:12 -0400

    puzzle 2

--- assets/scripts/puzzle/goal.gd
+++ assets/scripts/puzzle/goal.gd
@@ -6,6 +6,6 @@ signal part_complete(type: ProjectEnums.PartType)
 
 func _on_body_entered(body: PuzzlePart) -> void:
 	if body.type != ProjectEnums.PartType.NEUTRAL and body.target_goal == goal:
-		body.global_position = global_position
+		#body.global_position = global_position
 		part_complete.emit(body.type)
 		body.complete()
