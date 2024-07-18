9106785702861255502dd3069c0e82628facd8b9
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-18 16:31:51 -0400

    Puzzle sfx (#25)
    * puzzle pick up and drop sounds
    
    * puzzle success sound
    
    * increase end puzzle delay, clean up warnings
    
    * fix incorrect variable type
    
    ---------
    
    Co-authored-by: TABIBeck <becktiago98@gmail.com>

--- assets/scripts/npc/level_change.gd
+++ assets/scripts/npc/level_change.gd
@@ -4,7 +4,7 @@ extends GameMode
 @export var next_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	mode_type = GlobalEnums.GameState.OVERWORLD
 
 func set_active(active: bool) -> void:
