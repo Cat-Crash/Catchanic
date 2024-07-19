6e7e59171f92bbddf9ba8edf53da41e0e3bf007e
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-19 17:32:33 -0400

    clean up naming conventions

--- assets/scripts/Menu/menu.gd
+++ assets/scripts/Menu/menu.gd
@@ -5,12 +5,8 @@ func _on_start_pressed() -> void:
 	$fade_transition/fade_timer.start()
 	$fade_transition/AnimationPlayer.play("fade_in")
 
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_fade_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://assets/scenes/levels/level_1.tscn")
-	
