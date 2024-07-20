915dad9dc2b22f87849bd33a57acea1913fd814a
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-19 20:58:48 -0400

    fix fade done trigger

--- assets/scripts/menu/fade_transition.gd
+++ assets/scripts/menu/fade_transition.gd
@@ -3,11 +3,7 @@ extends ColorRect
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
-signal fade_done()
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#hide() # probably don't need this
+signal done()
 
 func fade_in() -> void:
 	animation_player.play("fade_in")
@@ -15,5 +11,5 @@ func fade_in() -> void:
 func fade_out() -> void:
 	animation_player.play("fade_out")
 
-func done() -> void:
-	fade_done.emit()
+func fade_done() -> void:
+	done.emit()
--- assets/scripts/menu/main_menu.gd
+++ assets/scripts/menu/main_menu.gd
@@ -4,7 +4,7 @@ extends Node
 
 func _on_start_pressed() -> void:
 	fade_transition.fade_in()
-	await fade_transition.done()	
+	await fade_transition.done
 	get_tree().change_scene_to_file("res://assets/scenes/levels/level_1.tscn")
 
 func _on_exit_pressed() -> void:
