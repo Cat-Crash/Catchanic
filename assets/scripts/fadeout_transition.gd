4103724121d58cfebdceb4d8e53c354d50147492
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-21 17:19:07 -0400

    fix missing active check

--- assets/scripts/fadeout_transition.gd
+++ assets/scripts/fadeout_transition.gd
@@ -5,7 +5,8 @@ func _ready() -> void:
 	mode_type = GlobalEnums.GameState.OVERWORLD
 
 func set_active(active: bool) -> void:
-	var fader : FadeTransition = GlobalState.overworld.fade_transition
-	fader.fade_out()
-	await fader.done
-	fader.fade_in()
+	if active:
+		var fader : FadeTransition = GlobalState.overworld.fade_transition
+		fader.fade_out()
+		await fader.done
+		fader.fade_in()
