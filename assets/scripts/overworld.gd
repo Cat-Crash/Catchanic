8a356104d4bd79e5ca1f26aee5d35b3cfcf92266
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-21 14:47:27 -0400

    updated title art and scene change transition

--- assets/scripts/overworld.gd
+++ assets/scripts/overworld.gd
@@ -1,11 +1,15 @@
 class_name Overworld
 extends GameMode
 
-var effects: Dictionary
-
 @onready var npc_parent: Node3D = $NPCs
 @onready var player : Player = $Player
 @onready var fade_transition = $"Lights and Sound/Fade Transition"
+@onready var bgm_player = $"Lights and Sound/BGMPlayer"
+
+@export var next_scene : PackedScene
+@export var exit_sound : AudioStream
+
+var effects: Dictionary
 
 func _ready() -> void:
 	fade_transition.fade_in()
@@ -19,3 +23,17 @@ func _ready() -> void:
 
 func set_active(active: bool) -> void:
 	player.process_mode = Node.PROCESS_MODE_INHERIT if active else PROCESS_MODE_DISABLED
+
+func exit_scene() -> void:
+	bgm_player.stop()
+	
+	fade_transition.fade_out()
+	await fade_transition.done
+	
+	if exit_sound:
+		bgm_player.stream = exit_sound
+		bgm_player.play()
+		await bgm_player.finished
+		fade_transition.fade_out()
+	
+	get_tree().change_scene_to_packed(next_scene)
