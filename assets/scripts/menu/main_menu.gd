9f6b0a1437bcb44acc58f49a361277c5823b3253
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-21 17:15:58 -0400

    menu bgm and start sound

--- assets/scripts/menu/main_menu.gd
+++ assets/scripts/menu/main_menu.gd
@@ -1,8 +1,15 @@
 extends Node
 
 @onready var fade_transition : FadeTransition = $"Fade Transition"
+@onready var bgm = $BGM
+
+@export var start_sound : AudioStream
 
 func _on_start_pressed() -> void:
+	bgm.stop()
+	bgm.stream = start_sound
+	bgm.play()
+	
 	fade_transition.fade_out()
 	await fade_transition.done
 	get_tree().change_scene_to_file("res://assets/scenes/levels/level_1.tscn")
