734273db9a75a9d03e29db0b88b4a54ae3edb990
Kevin Zhan <kevin.w.zhan@gmail.com>
2024-07-21 22:29:58 -0400

    add end screen, fix circular dependency, remove unecessary scene variable in level change node

--- assets/scripts/npc/level_change.gd
+++ assets/scripts/npc/level_change.gd
@@ -1,8 +1,6 @@
 class_name LevelChange
 extends GameMode
 
-@export var next_scene : PackedScene
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mode_type = GlobalEnums.GameState.OVERWORLD
